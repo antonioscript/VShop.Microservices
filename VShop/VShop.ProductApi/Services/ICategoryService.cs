0c3989e484b220ec0c9afa17d3edd0d283501c09
Antônio Rocha <antoniojunior159@gmail.com>
2024-03-28 22:24:29 -0300

    Create Controllers and Services

---
+++ VShop/VShop.ProductApi/Services/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿using VShop.ProductApi.DTOs;
+
+namespace VShop.ProductApi.Services;
+public interface ICategoryService
+{
+    Task<IEnumerable<CategoryDto>> GetCategories();
+    Task<IEnumerable<CategoryDto>> GetCategoriesProducts();
+    Task<CategoryDto> GetCategoryById(int id);
+    Task AddCategory(CategoryDto categoryDto);
+    Task UpdateCategory(CategoryDto categoryDto);
+    Task RemoveCategory(int id);
+}
