7a9c91cbad86287a1e61cd5ee646cfdefc56926f
Antônio Rocha <antoniojunior159@gmail.com>
2024-03-28 22:10:12 -0300

    Send project Vshop for the repository

---
+++ VShop/VShop.ProductApi/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using VShop.ProductApi.Models;
+
+namespace VShop.ProductApi.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAll();
+        Task<Product> GetById(int id);
+        Task<Product> Create(Product Product);
+        Task<Product> Update(Product Product);
+        Task<Product> Delete(int id);
+    }
+}
---
+++ VShop/VShop.ProductApi/Repositories/ProductRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VShop.ProductApi.Context;
+using VShop.ProductApi.Models;
+
+namespace VShop.ProductApi.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext _context;
+        public ProductRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetAll()
+        {
+            return await _context.Products
+                .ToListAsync();
+        }
+
+
+        public async Task<Product> GetById(int id)
+        {
+            return await _context.Products.
+                Where(c => c.Id == id)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<Product> Create(Product Product)
+        {
+            _context.Products.Add(Product);
+            await _context.SaveChangesAsync();
+
+            return Product;
+        }
+
+        public async Task<Product> Update(Product Product)
+        {
+            _context.Entry(Product).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return Product;
+        }
+
+        public async Task<Product> Delete(int id)
+        {
+            var Product = await GetById(id);
+            _context.Products.Remove(Product);
+            await _context.SaveChangesAsync();
+
+            return Product;
+        }
+
+        
+    }
+}
